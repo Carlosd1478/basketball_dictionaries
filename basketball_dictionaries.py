550493b71129c475c24315394163b966fa9d82b9
carlosd1478 <Carlos.delgado1478@live.com>
2023-03-09 10:48:09 -0500

    first commit

---
+++ basketball_dictionaries.py
@@ -0,0 +1,100 @@
+
+players = [
+    {
+        "name": "Kevin Durant",
+        "age": 34,
+        "position": "small forward",
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Jason Tatum",
+        "age": 24,
+        "position": "small forward",
+        "team": "Boston Celtics"
+    },
+    {
+        "name": "Kyrie Irving",
+        "age": 32,
+        "position": "Point Guard",
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Damian Lillard",
+        "age": 33,
+        "position": "Point Guard",
+        "team": "Portland Trailblazers"
+    },
+    {
+        "name": "Joel Embiid",
+        "age": 32,
+        "position": "Power Foward",
+        "team": "Philidelphia 76ers"
+    },
+    {
+        "name": "DeMar DeRozan",
+        "age": 32,
+        "position": "Shooting Guard",
+        "team": "Chicago Bulls"
+    }
+]
+
+class Player:
+    def __init__(self, data):
+        self.name = data['name']
+        self.age = data['age']
+        self.position = data['position']
+        self.team = data['team']
+
+    # * NINJA BONUS class mehotd
+
+    # @classmethod
+    # def add_players(cls, data):
+    #     player_objects = []
+    #     for dict in data:
+    #         player_objects.append(cls(dict))
+    #     return player_objects
+
+    def __repr__(self):
+        display = f"Player: {self.name}, {self.age} y/o, pos: {self.position}, team: {self.team}"
+        return display
+
+player1 = Player(players[0])
+player2 = Player(players[1])
+player3 = Player(players[2])
+player4 = Player(players[3])
+player5 = Player(players[4])
+player6 = Player(players[5])
+
+kevin = {
+    "name": "Kevin Durant",
+    "age": 34,
+    "position": "small forward",
+    "team": "Brooklyn Nets"
+}
+jason = {
+    "name": "Jason Tatum",
+    "age": 24,
+    "position": "small forward",
+    "team": "Boston Celtics"
+}
+kyrie = {
+    "name": "Kyrie Irving",
+    "age": 32,
+    "position": "Point Guard",
+    "team": "Brooklyn Nets"
+}
+
+# Create your Player instances here!
+player_jason = Player(jason)
+player_kevin = Player(kevin)
+player_kyrie = Player(kyrie)
+print(player_jason)
+print(player_kevin)
+print(player_kyrie)
+
+
+new_team = []
+for player_dict in players:
+    player = Player(player_dict)
+    new_team.append(player)
+print(new_team)
